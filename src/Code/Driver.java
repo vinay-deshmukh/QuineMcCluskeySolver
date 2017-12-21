a81d21ff7efbbae7b6d40dae74748c8e73c4d5e7
vinay-deshmukh <vinay_deshmukh@outlook.com>
2017-12-21 21:57:30 +0530

    Can create Prime Implicant Set

--- src/Code/Driver.java
+++ src/Code/Driver.java
@@ -26,18 +26,48 @@ public class Driver {
             }
         }
 
-        System.out.println("main, before step constructor");
         // Create a Step Object
         Step step = new Step(numbers);
         System.out.println("Step 0");
         step.display();
 
         System.out.println("\nStep 1\n");
-        System.out.println("Creating new step");
         Step step1 = step.createNextStep();
+
+        System.out.println("\n\n   DISPLAYING STEP 0");
+        step.display();
 
         System.out.println("\n\n   DISPLAYING STEP 1");
         step1.display();
+
+        Step step2 = step1.createNextStep();
+        System.out.println("\n\n   DISPLAYING STEP 2");
+        step2.display();
+
+        Step step3 = step2.createNextStep();
+        System.out.println("\n\n   DISPLAYING STEP 3");
+        step3.display();
+
+        System.out.println("\n\n Unticked terms");
+        System.out.println(Step.untickedTerms);
+
+
+        System.out.println(step.IsStepEmpty());
+        System.out.println(step1.IsStepEmpty());
+        System.out.println(step2.IsStepEmpty());
+        System.out.println(step3.IsStepEmpty());
+        //AUTO METHOD
+
+        Step stepN0 = new Step(numbers);
+        Step stepN1;
+        do {
+        stepN1 = stepN0.createNextStep();
+        stepN0 = stepN1;
+        }while(!stepN1.IsStepEmpty());
+        System.out.println("\n\n Unticked terms");
+        System.out.println(Step.untickedTerms);
+
+
 
 
     }
