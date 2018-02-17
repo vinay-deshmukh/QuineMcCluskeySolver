c21fae317248b4ed5f48af3b2794dbf7c22e5ebe
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-17 11:18:16 +0530

    Created tests

---
+++ src/Code/DriverTest1.java
@@ -0,0 +1,33 @@
+package Code;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class DriverTest1 {
+
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private DriverTest1 terminalview;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void displayPITable() {
+    }
+}
