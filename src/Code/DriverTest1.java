e1f66e854bd9d2555da15b7a78e09b3a4e609104
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-17 12:59:33 +0530

    Test works

--- src/Code/DriverTest1.java
+++ src/Code/DriverTest1.java
@@ -12,15 +12,24 @@ import static org.junit.Assert.*;
 public class DriverTest1 {
 
     private final PrintStream stdout = System.out;
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private DriverTest1 terminalview;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUpStreams() throws Exception {
+        System.setOut(new PrintStream(outContent));
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void restoreStreams() throws Exception {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void output_3()throws Exception{
+        System.out.print("3");
+        assertEquals("3", outContent.toString());
+
     }
 
     @Test
