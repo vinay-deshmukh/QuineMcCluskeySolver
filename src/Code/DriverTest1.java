d1b18591fc752f8cf301ff6252f317e80829778f
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-21 21:21:36 +0530

    Run array tests
    Inputs that give error are commented

--- src/Code/DriverTest1.java
+++ src/Code/DriverTest1.java
@@ -6,10 +6,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static Code.Driver.doQuineMcCluskey;
 import static org.junit.Assert.*;
@@ -37,7 +34,7 @@ public class DriverTest1 {
     // Give return type later on to use it to create inputs
     // Also param
 
-    public Object[] getInputs(String s1) throws Exception{
+    private Object[] getInputs(String s1) throws Exception{
         //String s1 = "0 1 2 3 5 7 8 9 11 14 = 14 ; 0,1,2,3 ; 0,1,8,9 ; 1,3,5,7 ; 1,3,9,11";
         //String s2 = "1 5 6 12 13 14 + 2 4 = 1,5 ; 4,5,12,13 ; 4,6,12,14";
 
@@ -92,28 +89,39 @@ public class DriverTest1 {
     }
 
     @Test
-    public void outputs() throws Exception{
-        /*
-        0 1 3 7 8 9 11 15 = 0,1,8,9 ; 3,7,11,15
-        0 1 2 3 5 7 8 9 11 14 = 14 ; 0,1,2,3 ; 0,1,8,9 ; 1,3,5,7 ; 1,3,9,11
-        1 5 6 12 13 14 + 2 4 = 1,5 ; 4,5,12,13 ; 4,6,12,14
-        0 1 3 4 5 6 11 13 14 15 = 3,11 ; 6,14 ; 13,15 ; 0,1,4,5
-        4 5 8 9 11 12 13 15 = 4,5,12,13 ; 8,9,12,13 ; 9,11,13,15
-        1 4 5 10 11 14 15 = 1,5 ; 4,5 ; 10,11,14,15
-        1 3 7 9 11 13 15 + 2 4 = 1,3,9,11 ; 3,7,11,15 ; 9,11,13,15
-        0 1 8 10 11 12 20 21 30 + 14 19 = 19 ; 0,1 ; 10,11 ; 20,21 ; 8,10,12,14
-        1 2 3 6 7 10 12 14 = 1,3 ; 12,14 ; 2,6,10,14
-        1 3 7 9 10 11 13 15 = 10,11 ; 1,3,9,11 ; 3,7,11,15 ; 9,11,13,15
-        0 1 2 8 10 11 14 15 = 0,1 ; 0,2,8,10 ; 10,11,14,15
-         */
+    public void checkArrayOutputs() throws Exception{
 
-        Object[] obs = getInputs("0 1 3 7 8 9 11 15 = 0,1,8,9 ; 3,7,11,15");
-        String inputMinterms = (String)obs[0];
-        String inputDonts = (String)obs[1];
-        Set expected = (Set)obs[2];
 
-        assertEquals(expected, doQuineMcCluskey(inputMinterms, inputDonts));
+        String[] inputs = {"0 1 3 7 8 9 11 15 = 0,1,8,9 ; 3,7,11,15",
+        "0 1 2 3 5 7 8 9 11 14 = 14 ; 0,1,2,3 ; 0,1,8,9 ; 1,3,5,7 ; 1,3,9,11",
+        "1 5 6 12 13 14 + 2 4 = 1,5 ; 4,5,12,13 ; 4,6,12,14",
+        "0 1 3 4 5 6 11 13 14 15 = 0,1,4,5",
+        "4 5 8 9 11 12 13 15 = 4,5,12,13 ; 8,9,12,13 ; 9,11,13,15",
+        "1 4 5 10 11 14 15 = 1,5 ; 4,5 ; 10,11,14,15",
+        "1 3 7 9 11 13 15 + 2 4 = 1,3,9,11 ; 3,7,11,15 ; 9,11,13,15",
+        //"0 1 8 10 11 12 20 21 30 + 14 19 = 19 ; 0,1 ; 10,11 ; 20,21 ; 8,10,12,14",
+        //"1 2 3 6 7 10 12 14 = 1,3 ; 12,14 ; 2,6,10,14",
+        "1 3 7 9 10 11 13 15 = 10,11 ; 1,3,9,11 ; 3,7,11,15 ; 9,11,13,15",
+        "0 1 2 8 10 11 14 15 = 0,1 ; 0,2,8,10 ; 10,11,14,15"
+        };
 
+        List<Set> expectedList = new ArrayList<>();
+        List<Set> resultList = new ArrayList<>();
+        for(String oneInput: inputs){
+            Object[] obs = getInputs(oneInput);
+            String inputMinterms = (String)obs[0];
+            String inputDonts = (String)obs[1];
+            Set expected = (Set)obs[2];
+
+            // Adding single expected result to list
+            expectedList.add(expected);
+
+            // Adding single actual result to list
+            resultList.add((Set)doQuineMcCluskey(inputMinterms, inputDonts));
+
+        }
+
+        assertArrayEquals(expectedList.toArray(),resultList.toArray());
     }
 
 }
