4b74ee3c214606a88c18a219a420f964c83defb0
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-21 19:17:15 +0530

    Create first test

--- src/Code/DriverTest1.java
+++ src/Code/DriverTest1.java
@@ -6,7 +6,11 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
+import static Code.Driver.doQuineMcCluskey;
 import static org.junit.Assert.*;
 
 public class DriverTest1 {
@@ -15,6 +19,8 @@ public class DriverTest1 {
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private DriverTest1 terminalview;
 
+    // Stream redirection is done since doQuineMcCluskey spits out lots of
+    // text which is irrelevant here
     @Before
     public void setUpStreams() throws Exception {
         System.setOut(new PrintStream(outContent));
@@ -22,7 +28,8 @@ public class DriverTest1 {
 
     @After
     public void restoreStreams() throws Exception {
-        System.setOut(System.out);
+        System.setOut(stdout);
+        System.out.println("test");
     }
 
     @Test
@@ -32,6 +39,7 @@ public class DriverTest1 {
 
     }
 
+    @Test
     public void outputs() throws Exception{
         /*
         0 1 3 7 8 9 11 15 =  0,1,8,9 ; 3,7,11,15
@@ -46,6 +54,22 @@ public class DriverTest1 {
         1 3 7 9 10 11 13 15 = 10,11 ; 1,3,9,11 ; 3,7,11,15 ; 9,11,13,15
         0 1 2 8 10 11 14 15 = 0,1 ; 0,2,8,10 ; 10,11,14,15
          */
+
+        Set result = (Set)doQuineMcCluskey("0 1 3 7 8 9 11 15","");
+        Set<Set> expected = new HashSet<>();
+
+        Set<Integer> a = new HashSet<>();
+        a.add(0); a.add(1); a.add(8); a.add(9);
+
+        expected.add(a);
+        a = new HashSet<>();
+        a.add(3); a.add(7); a.add(11); a.add(15);
+        expected.add(a);
+        stdout.println("Result: "+ result);
+        stdout.println("Expected: "+ expected);
+
+        assertEquals(expected, result);
+
     }
 
 }
